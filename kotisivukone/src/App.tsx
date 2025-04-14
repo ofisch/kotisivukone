063bd1ca6b9208f41f65c8a0f948845d41b31526
onni fischer <onni.fischer@gmail.com>
2025-04-14 13:47:41 +0300

    Enhance layout and styling

--- kotisivukone/src/App.tsx
+++ kotisivukone/src/App.tsx
@@ -24,47 +24,71 @@ function App() {
         </header>
         <Generator />
         <div className="info">
-          <div className="">
-            <h2>Mikä ihmeen kotisivukone?</h2>
-            <p>
-              Kotisivukone on työkalu, jolla voit luoda kotisivun helposti ja
-              nopeasti. <br />
-              <h4>Rakenna kotisivusi</h4>
-              <ul>
-                <li>kokoamalla se palikoista</li>
-                <li>lisäämällä sisältösi</li>
-              </ul>
-              <span style={{ fontWeight: "bold" }}>
-                Kun sivusi rakenne on valmis,{" "}
-              </span>
-              valitse sille vielä
-              <ul>
-                <li> ulkoasu</li>
-                <li>väriteema</li>
-                <li>
-                  ja paina{" "}
-                  <span
-                    style={{
-                      background:
-                        "-webkit-linear-gradient(0deg, #FC466B 0%, #3F5EFB 100%)",
-                      backgroundClip: "text",
-                      WebkitTextFillColor: "transparent",
-                      fontWeight: "bold",
-                    }}
-                  >
-                    Luo
-                  </span>
-                  -painiketta.{" "}
-                </li>
-              </ul>
-              <span style={{ fontWeight: "bold" }}>
-                Kotisivusi on valmis sekunneissa! 🚀
-              </span>
-            </p>
+          <div className="introduction">
+            <h1>
+              {" "}
+              Kotisivusi,{" "}
+              <span
+                style={{
+                  fontStyle: "italic",
+                  textDecoration: "underline",
+                  fontWeight: "bold",
+                }}
+              >
+                salamannopeasti.
+              </span>{" "}
+            </h1>
+            <p>Kokoa sivusi valmiista palikoista ja lisää oma sisältösi.</p>
+            <span style={{ fontWeight: "bold" }}>
+              Kun sivusi rakenne on valmis,{" "}
+            </span>
+            valitse sille vielä
+            <ul>
+              <li> ulkoasu</li>
+              <li>väriteema</li>
+              <li>
+                ja paina{" "}
+                <span
+                  style={{
+                    background:
+                      "-webkit-linear-gradient(0deg, #FC466B 0%, #3F5EFB 100%)",
+                    backgroundClip: "text",
+                    WebkitTextFillColor: "transparent",
+                    fontWeight: "bold",
+                  }}
+                >
+                  Luo
+                </span>
+                -painiketta.{" "}
+              </li>
+            </ul>
+            <h3 style={{ fontWeight: "bold" }}>
+              Kotisivusi on valmis sekunneissa! 🚀
+            </h3>
           </div>
+          <article>
+            <section className="features">
+              <p>
+                Kotisivukone luo ladattavan HTML-tiedoston, jonka voit julkaista
+                haluamallasi tavalla.
+                <br />
+                Kotisivukone ei tarjoa web-hosting palvelua.
+              </p>
+            </section>
+
+            <section className="showcase">
+              <h3>Tutustu Kotisivukoneella luotuihin sivuihin</h3>
+            </section>
+          </article>
         </div>
         <footer>
-          <ul style={{ listStyleType: "none", textAlign: "center" }}>
+          <ul
+            style={{
+              listStyleType: "none",
+              textAlign: "center",
+              marginTop: "4rem",
+            }}
+          >
             <li>tehty 🩵:lla Suomessa</li>
             <li>
               <a
